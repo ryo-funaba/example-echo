8806d28ef6cdf99da23233bb07ef10d8fc230a57
Ryo Funaba <ryo278.0417@gmail.com>
2023-01-03 17:07:15 +0900

    feat: echoを導入

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	p := os.Getenv("HTTP_PORT")
+
+	e.Logger.Fatal(e.Start(":" + p))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
